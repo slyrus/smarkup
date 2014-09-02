af79898d30dd1a1b675578ef784b8557237346b0
Cyrus Harmon <ch-github@bobobeach.com>
2014-09-02 15:03:24 -0700

    add exported symbols for various *document-... variables, render-as and with-keyword-args

--- defpackage.lisp
+++ defpackage.lisp
@@ -3,7 +3,22 @@
 
 (defpackage #:smarkup
   (:use #:cl #:asdf #:asdf-objects)
-  (:export #:object-from-file
+  (:export #:*document-title*
+           #:*document-titlerunning*
+           #:*document-subtitle*
+           #:*document-titlepage*
+           #:*document-author*
+           #:*document-tocauthor*
+           #:*document-authorrunning*
+           #:*document-address*
+           #:*document-institute*
+           #:*document-date*
+           #:*document-css-stylesheet*
+
+           #:render-as
+           #:with-keyword-args
+
+           #:object-from-file
            #:filtered-object
            #:object-latex-file
            #:pdf-file
@@ -15,6 +30,6 @@
            #:tiff-file
            #:jpeg-file
            #:png-file
-           
+
            #:smarkup-object-from-file))
 
