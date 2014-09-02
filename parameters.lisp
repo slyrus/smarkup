49d1fa71bf409ebe9be9311fdba2d05e99dd3d5e
Cyrus Harmon <ch-github@bobobeach.com>
2014-09-02 15:03:38 -0700

    remove latex stuff -- now in smarkup-latex

--- parameters.lisp
+++ parameters.lisp
@@ -15,15 +15,4 @@
 (defparameter *document-date* nil)
 (defparameter *document-css-stylesheet* nil)
 
-(defparameter *document-thesis* nil)
-(defparameter *document-degree-year* nil)
-(defparameter *document-degree-semester* nil)
-(defparameter *document-degree* nil)
-(defparameter *document-chair* nil)
-(defparameter *document-other-members* nil)
-(defparameter *document-number-of-members* nil)
-(defparameter *document-prev-degrees* nil)
-(defparameter *document-field* nil)
-(defparameter *document-campus* nil)
-
 (defparameter *document-render-type* nil)
